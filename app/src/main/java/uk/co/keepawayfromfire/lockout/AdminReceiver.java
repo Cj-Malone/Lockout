52e54cd39a35bd35086ffe45491e393c44e48f09
Cj Malone <CjMalone@mail.com>
2017-12-10 09:07:06 +0000

    Implement DeviceAdmin

---
+++ app/src/main/java/uk/co/keepawayfromfire/lockout/AdminReceiver.java
@@ -0,0 +1,32 @@
+package uk.co.keepawayfromfire.lockout;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by cj on 10/12/17.
+ */
+
+public class AdminReceiver extends DeviceAdminReceiver {
+
+    void showToast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+    
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        showToast(context, context.getString(R.string.admin_receiver_status_enabled));
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+        return context.getString(R.string.admin_receiver_status_disable_warning);
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        showToast(context, context.getString(R.string.admin_receiver_status_disabled));
+    }
+}
