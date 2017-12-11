432561df307bf5949918120243d08dd6514fc87a
Cj Malone <CjMalone@mail.com>
2017-12-11 07:43:21 +0000

    Add Unlock Receiver
    This needs an activity to register the receiver as android ignores the
    manifest in this case. :(

---
+++ app/src/main/java/uk/co/keepawayfromfire/lockout/MainActivity.java
@@ -0,0 +1,42 @@
+package uk.co.keepawayfromfire.lockout;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerUnlockReciever(this);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("unlock-data",
+                Context.MODE_PRIVATE);
+        String date = DateFormat.getDateTimeInstance().format(
+                new Date(sharedPreferences.getLong("last-unlock", 0)));
+
+        TextView textView1 = findViewById(R.id.textView1);
+
+        textView1.setText(date);
+
+    }
+
+    static void registerUnlockReciever(Context context) {
+        IntentFilter intentFilter = new IntentFilter("android.intent.action.USER_PRESENT");
+        intentFilter.addAction("android.intent.action.SCREEN_ON");
+        intentFilter.addAction("android.intent.action.BOOT_COMPLETED");
+        intentFilter.addAction("android.intent.action.SCREEN_OFF");
+
+        context.registerReceiver(new UnlockReceiver(), intentFilter);
+    }
+
+}
---
+++ app/src/main/java/uk/co/keepawayfromfire/lockout/UnlockReceiver.java
@@ -0,0 +1,25 @@
+package uk.co.keepawayfromfire.lockout;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import java.util.Date;
+
+/**
+ * Created by cj on 11/12/17.
+ */
+
+public class UnlockReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("unlock-data",
+                Context.MODE_PRIVATE);
+
+        long unlockTime = new Date().getTime();
+
+        sharedPreferences.edit().putLong("last-unlock", unlockTime).apply();
+    }
+}
