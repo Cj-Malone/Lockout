1c67dd6c8ff258f341e686083bc279e7ba5e235e
Cj Malone <CjMalone@mail.com>
2018-01-01 16:50:43 +0000

    Move to onPasswordSucceeded / watch-login
    Note, it basically dosen't work....
    onPasswordSucceeded is only called with a password and directly after onPasswordFailed.
    onPasswordFailed is only called with passwords, and if the failed attempt is same length as the real one.

--- app/src/main/java/uk/co/keepawayfromfire/lockout/AdminReceiver.java
+++ app/src/main/java/uk/co/keepawayfromfire/lockout/AdminReceiver.java
@@ -3,7 +3,11 @@ package uk.co.keepawayfromfire.lockout;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
 import android.widget.Toast;
+
+import java.util.Date;
 
 /**
  * Created by cj on 10/12/17.
@@ -17,16 +21,32 @@ public class AdminReceiver extends DeviceAdminReceiver {
 
     @Override
     public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
         showToast(context, context.getString(R.string.admin_receiver_status_enabled));
     }
 
     @Override
     public CharSequence onDisableRequested(Context context, Intent intent) {
+        super.onDisableRequested(context, intent);
         return context.getString(R.string.admin_receiver_status_disable_warning);
     }
 
     @Override
     public void onDisabled(Context context, Intent intent) {
+        super.onDisabled(context, intent);
         showToast(context, context.getString(R.string.admin_receiver_status_disabled));
+    }
+
+    @Override
+    public void onPasswordSucceeded(Context context, Intent intent, UserHandle user) {
+        super.onPasswordSucceeded(context, intent, user);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("unlock-data",
+                Context.MODE_PRIVATE);
+
+        long unlockTime = new Date().getTime();
+
+        sharedPreferences.edit().putLong("last-unlock", unlockTime).apply();
+
+        showToast(context, "Unlocked");
     }
 }
