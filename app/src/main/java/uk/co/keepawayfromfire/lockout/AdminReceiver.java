ce4d369481a6b561ba2e6d55069e8787f1035e1b
Cj Malone <CjMalone@mail.com>
2018-01-05 19:11:55 +0000

    Drop watch-login

--- app/src/main/java/uk/co/keepawayfromfire/lockout/AdminReceiver.java
+++ app/src/main/java/uk/co/keepawayfromfire/lockout/AdminReceiver.java
@@ -3,11 +3,7 @@ package uk.co.keepawayfromfire.lockout;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.UserHandle;
 import android.widget.Toast;
-
-import java.util.Date;
 
 /**
  * Created by cj on 10/12/17.
@@ -35,18 +31,5 @@ public class AdminReceiver extends DeviceAdminReceiver {
     public void onDisabled(Context context, Intent intent) {
         super.onDisabled(context, intent);
         showToast(context, context.getString(R.string.admin_receiver_status_disabled));
-    }
-
-    @Override
-    public void onPasswordSucceeded(Context context, Intent intent, UserHandle user) {
-        super.onPasswordSucceeded(context, intent, user);
-        SharedPreferences sharedPreferences = context.getSharedPreferences("unlock-data",
-                Context.MODE_PRIVATE);
-
-        long unlockTime = new Date().getTime();
-
-        sharedPreferences.edit().putLong("last-unlock", unlockTime).apply();
-
-        showToast(context, "Unlocked");
     }
 }
